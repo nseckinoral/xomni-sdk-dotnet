87707c8f88d2487032b51d5cd8bb93925af3b4f9
tugberkugurlu <tugberkugurlu@gmail.com>
2014-06-30 15:35:40 +0300

    powershell formatting fix

--- scripts/copy-binaries.ps1
+++ scripts/copy-binaries.ps1
@@ -1,23 +1,23 @@
 param(
-  [Parameter(Mandatory=$true)]
-	[string]$assemblyPath
+    [Parameter(Mandatory=$true)]
+    [string]$assemblyPath
 )
 
 $binariesPath = $env:XOMNI_SDK_BINARIES_DIR
 if($binariesPath -eq $null)
 {
-		Write-Host $binariesPath
-		$binariesPath = "c:\xomni-binaries"
+    Write-Host $binariesPath
+    $binariesPath = "c:\xomni-binaries"
 }
 
 if(Test-Path $binariesPath)
 {
-		$fileNameToCopy = (Split-Path $assemblyPath -Leaf)
-		$fullDestinationPath = (Join-Path $binariesPath $fileNameToCopy)
-		if(Test-Path $fullDestinationPath)
-		{
-				Remove-Item $fullDestinationPath
-		}
-		
-		Copy-Item $assemblyPath -Destination $binariesPath
+    $fileNameToCopy = (Split-Path $assemblyPath -Leaf)
+    $fullDestinationPath = (Join-Path $binariesPath $fileNameToCopy)
+    if(Test-Path $fullDestinationPath)
+    {
+        Remove-Item $fullDestinationPath
+    }
+    
+    Copy-Item $assemblyPath -Destination $binariesPath
 }
