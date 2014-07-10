f8daacb7b9b21eb64058eef780db864857284b72
ilkayilknur <ilkay@ilkayilknur.com>
2014-07-10 16:07:44 +0300

    Asset delete and relations methods added.

---
+++ src/XOMNI.SDK.Model.Private/Asset/AssetRelations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XOMNI.SDK.Model.Private.Asset
+{
+    public class AssetRelations
+    {
+        public List<int> RelatedItemIds { get; set; }
+        public List<int> RelatedBrandIds { get; set; }
+        public List<int> RelatedCategoryIds { get; set; }
+    }
+}
--- src/XOMNI.SDK.Private/Asset/AssetManagement.cs
+++ src/XOMNI.SDK.Private/Asset/AssetManagement.cs
@@ -8,11 +8,17 @@ using XOMNI.SDK.Model.Asset;
 using XOMNI.SDK.Core.Management;
 using XOMNI.SDK.Private.ApiAccess;
 using XOMNI.SDK.Private.ApiAccess.Asset;
+using XOMNI.SDK.Model.Private.Asset;
 
 namespace XOMNI.SDK.Private.Asset
 {
     public abstract class AssetManagement : ManagementBase
     {
+        private Assets AssetsApi;
+        public AssetManagement()
+        {
+            AssetsApi = new Assets();
+        }
         internal abstract AssetBase AssetApi { get; }
 
         public virtual Task<CountedCollectionContainer<Model.Private.Asset.Asset>> GetAssets(int skip, int take, string fileName = null)
@@ -23,6 +29,16 @@ namespace XOMNI.SDK.Private.Asset
         protected virtual Task<CountedCollectionContainer<T>> GetAssetsInternal<T>(int skip, int take, string fileName = null)
         {
             return AssetApi.Get<T>(skip, take, this.ApiCredential, fileName);
+        }
+
+        public virtual Task<AssetRelations> GetAssetRelationsAsync(int assetId)
+        {
+            return AssetsApi.GetAssetRelationsAsync(assetId, this.ApiCredential);
+        }
+
+        public virtual Task DeleteAssetAsync(int assetId, bool force)
+        {
+            return AssetsApi.DeleteAssetAsync(assetId, force, this.ApiCredential);
         }
     }
 }
