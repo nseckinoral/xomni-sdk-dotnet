668850c862350b06cbe182f69bbc5f7d9d56a83d
ilkayilknur <ilkay@ilkayilknur.com>
2014-08-12 17:56:17 +0300

    API implementation and namespace changes.
    Device type APIs implemeted and device API namespace changes.

---
+++ src/XOMNI.SDK.Management/ApiAccess/Company/DeviceTypes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XOMNI.SDK.Core.ApiAccess;
+using XOMNI.SDK.Model.Management.Company;
+
+namespace XOMNI.SDK.Management.ApiAccess.Company
+{
+    internal class DeviceTypes : CRUDApiAccessBase<DeviceType>
+    {
+        protected override string SingleOperationBaseUrl
+        {
+            get { return "/management/company/devicetypes/{0}"; }
+        }
+
+        protected override string ListOperationBaseUrl
+        {
+            get { return "/management/company/devicetypes"; }
+        }
+    }
+}
