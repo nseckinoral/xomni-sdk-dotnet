e8d1b2b3218a2a14fe88099ade702262e6c969ac
Gokhan Gulbiz <gokhan.gulbiz@xomni.com>
2014-08-14 14:36:07 +0300

    DynamicAttributeType management implemented.

---
+++ src/XOMNI.SDK.Model.Private/Catalog/DynamicAttributeTypeOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XOMNI.SDK.Model.Private.Catalog
+{
+    [Flags]
+    public enum DynamicAttributeTypeOptions : byte
+    {
+        NotInitialized = 0,
+        Retrievable,
+        Filterable,
+        Searchable,
+        Facetable,
+        Sortable,
+    }
+}
