992e7650d895d086eaaf42c0dd73ce318d841c5a
Gokhan Gulbiz <gokhan.gulbiz@xomni.com>
2014-08-21 15:12:59 +0300

    Private Store Management SDK implemented

---
+++ src/XOMNI.SDK.Model.Private/Company/Store.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XOMNI.SDK.Model.Private.Company
+{
+    public class Store : Model.Company.Store
+    {
+    }
+}
---
+++ src/XOMNI.SDK.Private/ApiAccess/Company/Store.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XOMNI.SDK.Core.ApiAccess;
+using XOMNI.SDK.Core.Providers;
+using XOMNI.SDK.Model;
+
+namespace XOMNI.SDK.Private.ApiAccess.Company
+{
+    public class Store : ApiAccessBase
+    {
+        protected override string SingleOperationBaseUrl
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        protected override string ListOperationBaseUrl
+        {
+            get { return "/private/company/stores"; }
+        }
+
+        public Task<CountedCollectionContainer<Model.Private.Company.Store>> GetAsync(int skip, int take, ApiBasicCredential credential)
+        {
+            var additionalParams = new Dictionary<string,string>();
+            additionalParams.Add("skip", skip.ToString());
+            additionalParams.Add("take", take.ToString());
+            return HttpProvider.GetAsync<CountedCollectionContainer<Model.Private.Company.Store>>(GenerateUrl(ListOperationBaseUrl, additionalParams), credential);
+        }
+    }
+}
