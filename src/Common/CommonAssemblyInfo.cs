7f0c23522a4053377070c55890d5c75348695622
Gokhan Gulbiz <gokhan.gulbiz@xomni.com>
2014-09-16 14:19:24 +0300

    AssemlyInformationalVersion updated.

--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -9,4 +9,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("2.1.0.0")]
 [assembly: AssemblyFileVersion("2.1.0.0")]
-[assembly: AssemblyInformationalVersion("2.1.0-alpha-002")]
+[assembly: AssemblyInformationalVersion("2.1.0.0")]
