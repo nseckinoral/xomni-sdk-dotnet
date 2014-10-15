1d38187e0deaaf695451cd310af2319e27eca022
Gokhan Gulbiz <gokhan.gulbiz@xomni.com>
2014-10-15 17:20:40 +0300

    Patch method added.

--- src/XOMNI.SDK.Core/Management/BaseCRUDPManagement.cs
+++ src/XOMNI.SDK.Core/Management/BaseCRUDPManagement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using XOMNI.SDK.Core.ApiAccess;
 
 namespace XOMNI.SDK.Core.Management
@@ -16,5 +17,15 @@ namespace XOMNI.SDK.Core.Management
 
         protected abstract CRUDPApiAccessBase<T> CRUDPApiAccess { get; }
 
+
+        /// <summary>
+        /// Updates an entity
+        /// </summary>
+        /// <param name="entity">Dynamic entity to be updated</param>
+        /// <returns>Updated entity</returns>
+        public virtual Task<T> UpdateAsync(dynamic entity)
+        {
+            return CRUDPApiAccess.PatchAsync(entity, base.ApiCredential);
+        }
     }
 }
