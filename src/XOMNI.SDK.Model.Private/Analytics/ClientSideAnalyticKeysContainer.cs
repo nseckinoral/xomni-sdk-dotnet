27dd2f9b9c4028fef5e58b8118d58070b55b091e
ilkayilknur <ilkay@ilkayilknur.com>
2014-12-16 16:03:23 +0200

    Client side analytics key methods implemented.

---
+++ src/XOMNI.SDK.Model.Private/Analytics/ClientSideAnalyticKeysContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOMNI.SDK.Model.Private.Analytics
+{
+    public class ClientSideAnalyticKeysContainer
+    {
+        public string ContinuationToken { get; set; }
+        public List<string> Keys { get; set; }
+    }
+}
--- src/XOMNI.SDK.Private/Analytics/Log/ClientAnalyticsManagement.cs
+++ src/XOMNI.SDK.Private/Analytics/Log/ClientAnalyticsManagement.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using XOMNI.SDK.Core.Management;
 using XOMNI.SDK.Core.Providers;
+using XOMNI.SDK.Model.Private.Analytics;
 using XOMNI.SDK.Model.Private.Analytics.Log;
 
 namespace XOMNI.SDK.Private.Analytics.Log
@@ -22,10 +23,20 @@ namespace XOMNI.SDK.Private.Analytics.Log
             return analyticsApiAccess.GetAllAsync(counterName, date, continuationKey, this.ApiCredential);
         }
 
+        public Task<ClientSideAnalyticKeysContainer> GetKeysAsync(string continuationKey = null)
+        {
+            return analyticsApiAccess.GetKeysAsync(continuationKey, this.ApiCredential);
+        }
+
         #region low level methods
         public XOMNIRequestMessage<AnalyticsLogContainer<ClientAnalyticsLog>> CreateGetAllRequest(string counterName, DateTime date, string continuationKey = null)
         {
             return analyticsApiAccess.CreateGetAllRequest<ClientAnalyticsLog>(counterName, date, continuationKey, this.ApiCredential);
+        }
+
+        public XOMNIRequestMessage<ClientSideAnalyticKeysContainer> CreateGetKeysRequest(string continuationKey = null)
+        {
+            return analyticsApiAccess.CreateGetKeysRequest(continuationKey, this.ApiCredential);
         }
         #endregion
     }
