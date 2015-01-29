401a9e9a7add635a8fb3317bda92d46cdad65a11
Sezgin Ege <mail@sezginege.com>
2015-01-29 19:28:59 +0200

    Client side validation added.Return type changed.
    Conflicts:
    	XOMNI.SDK/XOMNI.SDK.Public/Clients/Catalog/CategoryClient.cs

--- XOMNI.SDK/XOMNI.SDK.Public/Clients/Catalog/CategoryClient.cs
+++ XOMNI.SDK/XOMNI.SDK.Public/Clients/Catalog/CategoryClient.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using XOMNI.SDK.Public.Clients;
+using XOMNI.SDK.Public.Models;
 using XOMNI.SDK.Public.Models.Catalog;
 
 namespace XOMNI.SDK.Public.Clients.Catalog
@@ -15,23 +16,28 @@ namespace XOMNI.SDK.Public.Clients.Catalog
 
 		}
 
-		public async Task<List<Category>> GetAsync(bool includeMetadata, int imageAssetDetail, int videoAssetDetail, int documentAssetDetail)
+        public async Task<ApiResponse<CategoryTree<CategoryTreeItem>>> GetAsync(bool includeMetadata, AssetDetailType imageAssetDetail, AssetDetailType videoAssetDetail, AssetDetailType documentAssetDetail)
 		{
-			string path = string.Format("/catalog/categories?includeMetadata={0}&imageAssetDetail={1}&videoAssetDetail={2}&documentAssetDetail={3}", includeMetadata, imageAssetDetail, videoAssetDetail, documentAssetDetail);
+            string path = string.Format("/catalog/categories?includeMetadata={0}&imageAssetDetail={1}&videoAssetDetail={2}&documentAssetDetail={3}", includeMetadata, (int)imageAssetDetail, (int)videoAssetDetail, (int)documentAssetDetail);
 
 			using (var response = await Client.GetAsync(path).ConfigureAwait(false))
 			{
-                return await response.Content.ReadAsAsync<List<Category>>().ConfigureAwait(false);
+                return await response.Content.ReadAsAsync<ApiResponse<CategoryTree<CategoryTreeItem>>>().ConfigureAwait(false);
 			}
 		}
 
-		public async Task<CategoryTree<CategoryTreeItem>> GetAsync(int parentCategoryId, bool includeMetadata, int imageAssetDetail, int videoAssetDetail, int documentAssetDetail)
+        public async Task<ApiResponse<List<Category>>> GetAsync(int parentCategoryId, bool includeMetadata, AssetDetailType imageAssetDetail, AssetDetailType videoAssetDetail, AssetDetailType documentAssetDetail)
 		{
-			string path = string.Format("/catalog/categories?parentCategoryId={0}&includeMetadata={1}&imageAssetDetail={2}&videoAssetDetail={3}&documentAssetDetail={4}", parentCategoryId, includeMetadata, imageAssetDetail, videoAssetDetail, documentAssetDetail);
+            if(parentCategoryId<=0)
+            {
+                throw new ArgumentException("parentCategoryId");
+            }
+
+            string path = string.Format("/catalog/categories?parentCategoryId={0}&includeMetadata={1}&imageAssetDetail={2}&videoAssetDetail={3}&documentAssetDetail={4}", parentCategoryId, includeMetadata, (int)imageAssetDetail, (int)videoAssetDetail, (int)documentAssetDetail);
 
 			using (var response = await Client.GetAsync(path).ConfigureAwait(false))
 			{
-                return await response.Content.ReadAsAsync<CategoryTree<CategoryTreeItem>>().ConfigureAwait(false);
+                return await response.Content.ReadAsAsync<ApiResponse<List<Category>>>().ConfigureAwait(false);
 			}
 		}
 	}
