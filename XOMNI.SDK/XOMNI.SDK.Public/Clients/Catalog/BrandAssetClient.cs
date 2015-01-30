c5a890bfdaa9a6f096fc1981317355e466ae00dd
Sezgin Ege <mail@sezginege.com>
2015-01-30 15:59:51 +0200

    Client side validation added.
    Path generated.

--- XOMNI.SDK/XOMNI.SDK.Public/Clients/Catalog/BrandAssetClient.cs
+++ XOMNI.SDK/XOMNI.SDK.Public/Clients/Catalog/BrandAssetClient.cs
@@ -16,9 +16,26 @@ namespace XOMNI.SDK.Public.Clients.Catalog
 
 		}
 
-		public async Task<ApiResponse<List<ImageAsset>>> GetImagesAsync(int brandId, string metadataKey = null, string metadataValue = null, bool includeMetadata = false)
+		public async Task<ApiResponse<List<ImageAsset>>> GetImagesAsync(int brandId, string metadataKey = null, string metadataValue = null, AssetDetailType includeMetadata = AssetDetailType.IncludeAll)
 		{
-			string path = string.Format("/catalog/brands/{0}/images?metadataKey={1}&metadataValue={2}&includeMetadata={3}", brandId, metadataKey, metadataValue, includeMetadata);
+            if(brandId<=0)
+            {
+                throw new ArgumentException("brandId must be greater than or equal to zero");
+            }
+
+            string path = string.Format("/catalog/brands/{0}/images?", brandId);
+
+            if(!string.IsNullOrEmpty(metadataKey))
+            {
+                path += string.Format("metadataKey={0}&", metadataKey);
+            }
+
+            if (!string.IsNullOrEmpty(metadataValue))
+            {
+                path += string.Format("metadataValue={0}&", metadataValue);
+            }
+
+            path += string.Format("assetDetail={0}", includeMetadata);
 
 			using (var response = await Client.GetAsync(path).ConfigureAwait(false))
 			{
@@ -26,23 +43,56 @@ namespace XOMNI.SDK.Public.Clients.Catalog
 			}
 		}
 
-        public async Task<List<Asset>> GetVideosAsync(int brandId, string metadataKey = null, string metadataValue = null, bool includeMetadata = false)
+        public async Task<ApiResponse<List<Asset>>> GetVideosAsync(int brandId, string metadataKey = null, string metadataValue = null, AssetDetailType includeMetadata = AssetDetailType.IncludeAll)
 		{
-			string path = string.Format("/catalog/brands/{0}/videos?metadataKey={1}&metadataValue={2}&includeMetadata={3}", brandId, metadataKey, metadataValue, includeMetadata);
+            if (brandId <= 0)
+            {
+                throw new ArgumentException("brandId must be greater than or equal to zero");
+            }
+            string path = string.Format("/catalog/brands/{0}/videos?", brandId);
+
+            if (!string.IsNullOrEmpty(metadataKey))
+            {
+                path += string.Format("metadataKey={0}&", metadataKey);
+            }
+
+            if (!string.IsNullOrEmpty(metadataValue))
+            {
+                path += string.Format("metadataValue={0}&", metadataValue);
+            }
+
+            path += string.Format("assetDetail={0}", includeMetadata);
 
 			using (var response = await Client.GetAsync(path).ConfigureAwait(false))
 			{
-                return await response.Content.ReadAsAsync<List<Asset>>().ConfigureAwait(false);
+                return await response.Content.ReadAsAsync<ApiResponse<List<Asset>>>().ConfigureAwait(false);
 			}
 		}
 
-        public async Task<List<Asset>> GetDocumentsAsync(int brandId, string metadataKey = null, string metadataValue = null, bool includeMetadata = false)
+        public async Task<ApiResponse<List<Asset>>> GetDocumentsAsync(int brandId, string metadataKey = null, string metadataValue = null, AssetDetailType includeMetadata = AssetDetailType.IncludeAll)
 		{
-			string path = string.Format("/catalog/brands/{0}/documents?metadataKey={1}&metadataValue={2}&includeMetadata={3}", brandId, metadataKey, metadataValue, includeMetadata);
+            if (brandId <= 0)
+            {
+                throw new ArgumentException("brandId must be greater than or equal to zero");
+            }
+
+            string path = string.Format("/catalog/brands/{0}/documents?", brandId);
+
+            if (!string.IsNullOrEmpty(metadataKey))
+            {
+                path += string.Format("metadataKey={0}&", metadataKey);
+            }
+
+            if (!string.IsNullOrEmpty(metadataValue))
+            {
+                path += string.Format("metadataValue={0}&", metadataValue);
+            }
+
+            path += string.Format("assetDetail={0}", includeMetadata);
 
 			using (var response = await Client.GetAsync(path).ConfigureAwait(false))
 			{
-                return await response.Content.ReadAsAsync<List<Asset>>().ConfigureAwait(false);
+                return await response.Content.ReadAsAsync<ApiResponse<List<Asset>>>().ConfigureAwait(false);
 			}
 		}
 	}
