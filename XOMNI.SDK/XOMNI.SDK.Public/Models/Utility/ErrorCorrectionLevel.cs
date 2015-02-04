1326fd3839987c7ba92ab5c2f949cbc07f4f7d18
Gokhan Gulbiz <ggulbiz@gmail.com>
2015-02-04 19:03:50 +0200

    ErrorCorrectionLevel enum added.

---
+++ XOMNI.SDK/XOMNI.SDK.Public/Models/Utility/ErrorCorrectionLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOMNI.SDK.Public.Models.Utility
+{
+    public enum ErrorCorrectionLevel
+    {
+        Low = 0,
+        Medium = 1,
+        Quartile = 2,
+        High = 3
+    }
+}
