324c05faf1dcfe898853ae7a3bafa5c14e15b42a
Sezgin Ege <mail@sezginege.com>
2015-02-05 11:51:32 +0200

    Client side validation added.

--- XOMNI.SDK/XOMNI.SDK.Public/Clients/Company/StoreClient.cs
+++ XOMNI.SDK/XOMNI.SDK.Public/Clients/Company/StoreClient.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using XOMNI.SDK.Public.Clients;
 using XOMNI.SDK.Public.Models;
 using XOMNI.SDK.Public.Models.Company;
+using XOMNI.SDK.Public.Extensions;
 
 namespace XOMNI.SDK.Public.Clients.Company
 {
@@ -17,6 +18,11 @@ namespace XOMNI.SDK.Public.Clients.Company
 
         public async Task<ApiResponse<PaginatedContainer<Store>>> GetAsync(string locationInfo, int searchDistance, int skip, int take)
 		{
+            Validator.For(locationInfo, "LocationInfo").NotNull().IsEmpty().IsContain(';');
+            Validator.For(searchDistance, "SearchDistance").InRange(0, 1);
+            Validator.For(skip, "Skip").IsGreaterThanOrEqual(1);
+            Validator.For(skip, "Take").IsGreaterThanOrEqual(1);
+
 			string path = string.Format("/company/stores?locationInfo={0}&searchDistance={1}&skip={2}&take={3}", locationInfo, searchDistance, skip, take);
 
 			using (var response = await Client.GetAsync(path).ConfigureAwait(false))
