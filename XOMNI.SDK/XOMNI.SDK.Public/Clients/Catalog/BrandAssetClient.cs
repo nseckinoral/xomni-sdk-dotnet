0f7390951533c33d9f149a90f1685f1ef860cf23
Sezgin Ege <mail@sezginege.com>
2015-02-09 11:53:48 +0200

    Client side validation edited.

--- XOMNI.SDK/XOMNI.SDK.Public/Clients/Catalog/BrandAssetClient.cs
+++ XOMNI.SDK/XOMNI.SDK.Public/Clients/Catalog/BrandAssetClient.cs
@@ -5,95 +5,88 @@ using System.Threading.Tasks;
 using XOMNI.SDK.Public.Clients;
 using XOMNI.SDK.Public.Models;
 using XOMNI.SDK.Public.Models.Catalog;
+using XOMNI.SDK.Public.Extensions;
 
 namespace XOMNI.SDK.Public.Clients.Catalog
 {
-	public class BrandAssetClient : BaseClient
-	{
-		public BrandAssetClient(HttpClient httpClient)
-			: base(httpClient)
-		{
+    public class BrandAssetClient : BaseClient
+    {
+        public BrandAssetClient(HttpClient httpClient)
+            : base(httpClient)
+        {
 
-		}
+        }
 
-		public async Task<ApiResponse<List<ImageAsset>>> GetImagesAsync(int brandId, string metadataKey = null, string metadataValue = null, AssetDetailType includeMetadata = AssetDetailType.IncludeAll)
-		{
-            if(brandId<=0)
-            {
-                throw new ArgumentException("brandId must be greater than or equal to zero");
-            }
+        public async Task<ApiResponse<List<ImageAsset>>> GetImagesAsync(int brandId, string metadataKey = null, string metadataValue = null, AssetDetailType includeMetadata = AssetDetailType.IncludeOnlyDefaultWithMetadata)
+        {
+            Validator.For(brandId, "brandId").IsGreaterThanOrEqual(1);
 
             string path = string.Format("/catalog/brands/{0}/images?", brandId);
 
-            if(!string.IsNullOrEmpty(metadataKey))
+            if (!string.IsNullOrEmpty(metadataKey) && !string.IsNullOrEmpty(metadataValue))
             {
-                path += string.Format("metadataKey={0}&", metadataKey);
+                path += string.Format("metadataKey={0}&metadataValue={1}&", metadataKey, metadataValue);
+            }
+            else if (!string.IsNullOrEmpty(metadataKey) || !string.IsNullOrEmpty(metadataValue))
+            {
+                Validator.For(metadataKey, "metadataKey").IsNotNullOrEmpty();
+                Validator.For(metadataValue, "metadataValue").IsNotNullOrEmpty();
             }
 
-            if (!string.IsNullOrEmpty(metadataValue))
+            path += string.Format("assetDetail={0}", (int)includeMetadata);
+
+            using (var response = await Client.GetAsync(path).ConfigureAwait(false))
             {
-                path += string.Format("metadataValue={0}&", metadataValue);
-            }
-
-            path += string.Format("assetDetail={0}", includeMetadata);
-
-			using (var response = await Client.GetAsync(path).ConfigureAwait(false))
-			{
                 return await response.Content.ReadAsAsync<ApiResponse<List<ImageAsset>>>().ConfigureAwait(false);
-			}
-		}
-
-        public async Task<ApiResponse<List<Asset>>> GetVideosAsync(int brandId, string metadataKey = null, string metadataValue = null, AssetDetailType includeMetadata = AssetDetailType.IncludeAll)
-		{
-            if (brandId <= 0)
-            {
-                throw new ArgumentException("brandId must be greater than or equal to zero");
             }
+        }
+
+        public async Task<ApiResponse<List<Asset>>> GetVideosAsync(int brandId, string metadataKey = null, string metadataValue = null, AssetDetailType includeMetadata = AssetDetailType.IncludeOnlyDefaultWithMetadata)
+        {
+            Validator.For(brandId, "brandId").IsGreaterThanOrEqual(1);
+
             string path = string.Format("/catalog/brands/{0}/videos?", brandId);
 
-            if (!string.IsNullOrEmpty(metadataKey))
+            if (!string.IsNullOrEmpty(metadataKey) && !string.IsNullOrEmpty(metadataValue))
             {
-                path += string.Format("metadataKey={0}&", metadataKey);
+                path += string.Format("metadataKey={0}&metadataValue={1}&", metadataKey, metadataValue);
+            }
+            else if (!string.IsNullOrEmpty(metadataKey) || !string.IsNullOrEmpty(metadataValue))
+            {
+                Validator.For(metadataKey, "metadataKey").IsNotNullOrEmpty();
+                Validator.For(metadataValue, "metadataValue").IsNotNullOrEmpty();
             }
 
-            if (!string.IsNullOrEmpty(metadataValue))
+            path += string.Format("assetDetail={0}", (int)includeMetadata);
+
+            using (var response = await Client.GetAsync(path).ConfigureAwait(false))
             {
-                path += string.Format("metadataValue={0}&", metadataValue);
-            }
-
-            path += string.Format("assetDetail={0}", includeMetadata);
-
-			using (var response = await Client.GetAsync(path).ConfigureAwait(false))
-			{
                 return await response.Content.ReadAsAsync<ApiResponse<List<Asset>>>().ConfigureAwait(false);
-			}
-		}
-
-        public async Task<ApiResponse<List<Asset>>> GetDocumentsAsync(int brandId, string metadataKey = null, string metadataValue = null, AssetDetailType includeMetadata = AssetDetailType.IncludeAll)
-		{
-            if (brandId <= 0)
-            {
-                throw new ArgumentException("brandId must be greater than or equal to zero");
             }
+        }
+
+        public async Task<ApiResponse<List<Asset>>> GetDocumentsAsync(int brandId, string metadataKey = null, string metadataValue = null, AssetDetailType includeMetadata = AssetDetailType.IncludeOnlyDefaultWithMetadata)
+        {
+            Validator.For(brandId, "brandId").IsGreaterThanOrEqual(1);
 
             string path = string.Format("/catalog/brands/{0}/documents?", brandId);
 
-            if (!string.IsNullOrEmpty(metadataKey))
+            if (!string.IsNullOrEmpty(metadataKey) && !string.IsNullOrEmpty(metadataValue))
             {
-                path += string.Format("metadataKey={0}&", metadataKey);
+                path += string.Format("metadataKey={0}&metadataValue={1}&", metadataKey, metadataValue);
+            }
+            else if (!string.IsNullOrEmpty(metadataKey) || !string.IsNullOrEmpty(metadataValue))
+            {
+                Validator.For(metadataKey, "metadataKey").IsNotNullOrEmpty();
+                Validator.For(metadataValue, "metadataValue").IsNotNullOrEmpty();
             }
 
-            if (!string.IsNullOrEmpty(metadataValue))
+            path += string.Format("assetDetail={0}", (int)includeMetadata);
+
+            using (var response = await Client.GetAsync(path).ConfigureAwait(false))
             {
-                path += string.Format("metadataValue={0}&", metadataValue);
-            }
-
-            path += string.Format("assetDetail={0}", includeMetadata);
-
-			using (var response = await Client.GetAsync(path).ConfigureAwait(false))
-			{
                 return await response.Content.ReadAsAsync<ApiResponse<List<Asset>>>().ConfigureAwait(false);
-			}
-		}
-	}
+            }
+        }
+    }
 }
