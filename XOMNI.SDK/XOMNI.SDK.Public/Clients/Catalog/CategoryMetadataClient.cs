51a0fc3691d0e747c7eb718a382b1c2c0421d229
Sezgin Ege <mail@sezginege.com>
2015-02-09 13:42:04 +0200

    Client side validation added. Return type changed.

--- XOMNI.SDK/XOMNI.SDK.Public/Clients/Catalog/CategoryMetadataClient.cs
+++ XOMNI.SDK/XOMNI.SDK.Public/Clients/Catalog/CategoryMetadataClient.cs
@@ -3,6 +3,8 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using XOMNI.SDK.Public.Clients;
 using XOMNI.SDK.Public.Models;
+using XOMNI.SDK.Public.Extensions;
+using System.Collections.Generic;
 
 namespace XOMNI.SDK.Public.Clients.Catalog
 {
@@ -14,13 +16,15 @@ namespace XOMNI.SDK.Public.Clients.Catalog
 
 		}
 
-		public async Task<ApiResponse<Metadata>> GetAsync(int categoryId)
+		public async Task<ApiResponse<List<Metadata>>> GetAsync(int categoryId)
 		{
+            Validator.For(categoryId, "categoryId").IsGreaterThanOrEqual(1);
+
 			string path = string.Format("/catalog/categorymetadata?categoryId={0}", categoryId);
 
 			using (var response = await Client.GetAsync(path).ConfigureAwait(false))
 			{
-				return await response.Content.ReadAsAsync<ApiResponse<Metadata>>().ConfigureAwait(false);
+				return await response.Content.ReadAsAsync<ApiResponse<List<Metadata>>>().ConfigureAwait(false);
 			}
 		}
 	}
