d2c914986675b5079982e71d0bb67e7d82f53053
Sezgin Ege <mail@sezginege.com>
2015-02-10 12:39:35 +0200

    Added ToString method.

--- XOMNI.SDK/XOMNI.SDK.Public/Models/Location.cs
+++ XOMNI.SDK/XOMNI.SDK.Public/Models/Location.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XOMNI.SDK.Public.Extensions;
 
 namespace XOMNI.SDK.Public.Models
 {
@@ -10,5 +12,16 @@ namespace XOMNI.SDK.Public.Models
     {
         public double? Longitude { get; set; }
         public double? Latitude { get; set; }
+
+        public override string ToString()
+        {
+            Validator.For(Longitude, "Longitude").IsNotNull();
+            Validator.For(Latitude, "Latitude").IsNotNull();
+
+            string longitude = string.Format(new CultureInfo("en-US"),"{0:00.000000}", Longitude);
+            string latitude = string.Format(new CultureInfo("en-US"),"{0:00.000000}", Latitude);
+
+            return string.Format("{0};{1}", longitude, latitude);
+        }
     }
 }
