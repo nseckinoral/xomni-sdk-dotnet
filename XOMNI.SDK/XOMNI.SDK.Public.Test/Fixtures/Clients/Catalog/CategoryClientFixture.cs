db66800d83b16bac32560a63a85dc7d4aa617be8
Efe Engin <enginbehcetefe@gmail.com>
2015-02-10 12:56:56 +0200

    CatagoryClient Fixture added.

---
+++ XOMNI.SDK/XOMNI.SDK.Public.Test/Fixtures/Clients/Catalog/CategoryClientFixture.cs
@@ -0,0 +1,412 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using XOMNI.SDK.Public.Clients.Catalog;
+using XOMNI.SDK.Public.Models;
+using XOMNI.SDK.Public.Models.Catalog;
+using XOMNI.SDK.Public.Test.Helpers;
+
+namespace XOMNI.SDK.Public.Test.Fixtures.Clients.Catalog
+{
+    [TestClass]
+    public class CategoryClientFixture : BaseClientFixture<CategoryClient>
+    {
+        #region GetAsyncCategoryTree
+
+        const string validAPIResponseForCategoryTreeItem = @"{  
+           'Data':{  
+              'Categories':[  
+                 {  
+                    'Id':1,
+                    'Name':'Sample Category 0',
+                    'ShortDescription':'Sample Short Description',
+                    'LongDescription':'Sample Long Description',
+                    'CategoryMetadata':[  
+                       {  
+                          'Key':'Key0',
+                          'Value':'Value0'
+                       },
+                       {  
+                          'Key':'Key1',
+                          'Value':'Value1'
+                       }
+                    ],
+                    'ImageAssets':[  
+                       {  
+                          'ResizedAssets':[  
+                             {  
+                                'ImageSizeProfile':{  
+                                   'Id':1,
+                                   'Height':300,
+                                   'Width':500
+                                },
+                                'AssetUrl':'https://xomni.blob.core.windows.net/images/6D16EE1F-73A1-4BB0-9BAA-8FD27C0B3568'
+                             },
+                             {  
+                                'ImageSizeProfile':{  
+                                   'Id':2,
+                                   'Height':600,
+                                   'Width':1000
+                                },
+                                'AssetUrl':'https://xomni.blob.core.windows.net/images/6D16EE1F-73A1-4BB0-9BAA-8FD27C0B3568'
+                             },
+                             {  
+                                'ImageSizeProfile':{  
+                                   'Id':3,
+                                   'Height':900,
+                                   'Width':1500
+                                },
+                                'AssetUrl':'https://xomni.blob.core.windows.net/images/6D16EE1F-73A1-4BB0-9BAA-8FD27C0B3568'
+                             }
+                          ],
+                          'AssetMetadata':[  
+                             {  
+                                'Key':'assetmetadatakey1',
+                                'Value':'assetmetadatavalue1'
+                             },
+                             {  
+                                'Key':'assetmetadatakey2',
+                                'Value':'assetmetadatavalu2'
+                             },
+                             {  
+                                'Key':'assetmetadatakey3',
+                                'Value':'assetmetadatavalue3'
+                             }
+                          ],
+                          'AssetId':1,
+                          'AssetUrl':'https://xomni.blob.core.windows.net/images/5e2dd075-957f-4884-8992-030d1eabcc79',
+                          'IsDefault':true
+                       }
+                    ],
+                    'DocumentAssets':[  
+                       {  
+                          'AssetMetadata':[  
+                             {  
+                                'Key':'assetmetadatakey2',
+                                'Value':'assetmetadatavalue2'
+                             },
+                             {  
+                                'Key':'assetmetadatakey5',
+                                'Value':'assetmetadatavalue5'
+                             },
+                             {  
+                                'Key':'assetmetadatakey6',
+                                'Value':'assetmetadatavalue6'
+                             }
+                          ],
+                          'AssetId':3,
+                          'AssetUrl':'https://xomni.blob.core.windows.net/documents/5e2dd075-957f-4884-8992-030d1eabcc79'
+                       }
+                    ],
+                    'VideoAssets':[  
+
+                    ],
+                    'CategoryTreeItems':[  
+                       {  
+                          'Id':11,
+                          'Name':'Sample Sub Category 0',
+                          'ShortDescription':'Sub category sample short description',
+                          'LongDescription':'Sub category sample long description',
+                          'CategoryMetadata':[  
+
+                          ],
+                          'ImageAssets':[  
+
+                          ],
+                          'DocumentAssets':[  
+
+                          ],
+                          'VideoAssets':[  
+
+                          ],
+                          'CategoryTreeItems':[  
+
+                          ]
+                       },
+                       {  
+                          'Id':12,
+                          'Name':'Sample Sub Category 1',
+                          'ShortDescription':'Sample Sub Category 1 Short Description',
+                          'LongDescription':'Sample Sub Category 1 Long Description',
+                          'CategoryMetadata':[  
+
+                          ],
+                          'CategoryTreeItems':[  
+
+                          ],
+                          'ImageAssets':[  
+
+                          ],
+                          'DocumentAssets':[  
+
+                          ],
+                          'VideoAssets':[  
+
+                          ]
+                       }
+                    ]
+                 },
+                 {  
+                    'Id':2,
+                    'Name':'Sample Category 1',
+                    'ShortDescription':'Sample Category 1 Short Description',
+                    'LongDescription':'Sample Category 1 Long Description',
+                    'CategoryMetadata':[  
+                       {  
+                          'Key':'categorymetadata0key',
+                          'Value':'categorymetadata0value'
+                       },
+                       {  
+                          'Key':'categorymetadata1key',
+                          'Value':'categorymetadata1value'
+                       }
+                    ],
+                    'CategoryTreeItems':[  
+
+                    ],
+                    'ImageAssets':[  
+
+                    ],
+                    'DocumentAssets':[  
+
+                    ],
+                    'VideoAssets':[  
+
+                    ]
+                 }
+              ]
+           }
+        }";
+
+        readonly HttpResponseMessage validHttpResponseMessageForCategoryTreeItem = new HttpResponseMessage()
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = new MockedJsonContent(validAPIResponseForCategoryTreeItem)
+        };
+        
+        [TestMethod, TestCategory("CategoryClient"), TestCategory("GetAsync"), TestCategory("HTTP.GET")]
+        public async Task GetAsyncCategoryTreeResponseParseTest()
+        {
+            await base.ResponseParseTestAsync(
+                (CategoryClient c) => c.GetAsync(false, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata),
+                validHttpResponseMessage,
+                validAPIResponseForCategoryTreeItem
+                );
+        }
+
+        [TestMethod, TestCategory("CategoryClient"), TestCategory("GetAsync"), TestCategory("HTTP.GET")]
+        public async Task GetAsyncCategoryTreeHttpMethodTest()
+        {
+            await base.HttpMethodTestAsync(
+                (CategoryClient c) => c.GetAsync(false, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata),
+                HttpMethod.Get
+                );
+        }
+
+        [TestMethod, TestCategory("CategoryClient"), TestCategory("GetAsync"), TestCategory("HTTP.GET")]
+        public async Task GetAsyncCategoryTreeUriCheckTest()
+        {
+            await base.UriTestAsync(
+              (CategoryClient p) => p.GetAsync(false, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata),
+              string.Format("/catalog/categories?includeMetadata={0}&imageAssetDetail={1}&videoAssetDetail={2}&documentAssetDetail={3}", false, 4,4,4));
+        }
+
+        [TestMethod, TestCategory("CategoryClient"), TestCategory("GetAsync"), TestCategory("HTTP.GET")]
+        public async Task GetAsyncCategoryTreeHeadersTest()
+        {
+            await base.DefaultRequestHeadersTestAsync(
+                (CategoryClient c) => c.GetAsync(false, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata)
+            );
+        }
+
+        [TestMethod, TestCategory("CategoryClient"), TestCategory("GetAsync"), TestCategory("HTTP.GET")]
+        public async Task GetAsyncCategoryTreeNotFoundTest()
+        {
+            var expectedExceptionResult = JsonConvert.DeserializeObject<ExceptionResult>(genericErrorResponse);
+            expectedExceptionResult.HttpStatusCode = HttpStatusCode.NotFound;
+
+            await base.APIExceptionResponseTestAsync(
+              (CategoryClient t) => t.GetAsync(false, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata),
+              notFoundHttpResponseMessage,
+              expectedExceptionResult
+            );
+        }
+        #endregion
+
+        #region GetAsyncCategory
+
+        const string validAPIResponseForCategory = @"{
+            'Data': [
+                {
+                    'Id': 1,
+                    'Name': 'SampleCategory0',
+                    'ShortDescription': 'SampleCategory0',
+                    'LongDescription': 'SampleCategory0',
+                    'SubCategoryCount': 2,
+                    'CategoryMetadata': [
+                        {
+                            'Key': 'categorymetadatakey1',
+                            'Value': 'categorymetadatavalue1'
+                        },
+                        {
+                            'Key': 'categorymetadatakey2',
+                            'Value': 'categorymetadatavalue2'
+                        }
+                    ],
+                    'ImageAssets': [
+                        {
+                            'ResizedAssets': [
+                                {
+                                    'ImageSizeProfile': {
+                                        'Id': 1,
+                                        'Height': 300,
+                                        'Width': 500
+                                    },
+                                    'AssetUrl': 'https: //xomni.blob.core.windows.net/images/6D16EE1F-73A1-4BB0-9BAA-8FD27C0B3568'
+                                },
+                                {
+                                    'ImageSizeProfile': {
+                                        'Id': 2,
+                                        'Height': 600,
+                                        'Width': 1000
+                                    },
+                                    'AssetUrl': 'https: //xomni.blob.core.windows.net/images/6D16EE1F-73A1-4BB0-9BAA-8FD27C0B3568'
+                                },
+                                {
+                                    'ImageSizeProfile': {
+                                        'Id': 3,
+                                        'Height': 900,
+                                        'Width': 1500
+                                    },
+                                    'AssetUrl': 'https: //xomni.blob.core.windows.net/images/6D16EE1F-73A1-4BB0-9BAA-8FD27C0B3568'
+                                }
+                            ],
+                            'AssetMetadata': [
+                                {
+                                    'Key': 'assetmetadatakey1',
+                                    'Value': 'assetmetadatavalue1'
+                                },
+                                {
+                                    'Key': 'assetmetadatakey2',
+                                    'Value': 'assetmetadatavalue2'
+                                },
+                                {
+                                    'Key': 'assetmetadatakey3',
+                                    'Value': 'assetmetadatavalue3'
+                                }
+                            ],
+                            'AssetId': 1,
+                            'AssetUrl': 'https: //xomni.blob.core.windows.net/images/5e2dd075-957f-4884-8992-030d1eabcc79',
+                            'IsDefault': true
+                        }
+                    ],
+                    'DocumentAssets': [
+                        {
+                            'AssetMetadata': [
+                                {
+                                    'Key': 'assetmetadatakey3',
+                                    'Value': 'assetmetadatavalue3'
+                                },
+                                {
+                                    'Key': 'assetmetadatakey6',
+                                    'Value': 'assetmetadatavalue6'
+                                },
+                                {
+                                    'Key': 'assetmetadatakey5',
+                                    'Value': 'assetmetadatavalue5'
+                                }
+                            ],
+                            'AssetId': 3,
+                            'AssetUrl': 'https: //xomni.blob.core.windows.net/images/5e2dd075-957f-4884-8992-030d1eabcc79'
+                        }
+                    ],
+                    'VideoAssets': [
+                
+                    ],
+                    'ParentCategoryId': 1,
+                    'ParentCategoryName': 'Shoes'
+                },
+                {
+                    'Id': 2,
+                    'Name': 'SampleCategory1',
+                    'ShortDescription': 'SampleCategory1ShortDescription',
+                    'LongDescription': 'SampleCategory1LongDescription: ',
+                    'SubCategoryCount': 0,
+                    'CategoryMetadata': [
+                        {
+                            'Key': 'categorymetadatakey1',
+                            'Value': 'categorymetadatavalue1'
+                        },
+                        {
+                            'Key': 'categorymetadatakey5',
+                            'Value': 'categorymetadatavalue5'
+                        }
+                    ],
+                    'ParentCategoryId': 1,
+                    'ParentCategoryName': 'Shoes'
+                }
+            ]
+        }";
+
+        readonly HttpResponseMessage validHttpResponseMessage = new HttpResponseMessage()
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = new MockedJsonContent(validAPIResponseForCategory)
+        };
+
+        [TestMethod, TestCategory("CategoryClient"), TestCategory("GetAsync"), TestCategory("HTTP.GET")]
+        public async Task GetAsyncResponseParseTest()
+        {
+            await base.ResponseParseTestAsync(
+                (CategoryClient c) => c.GetAsync(1, false, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata),
+                validHttpResponseMessage,
+                validAPIResponseForCategory
+                );
+        }
+
+        [TestMethod, TestCategory("CategoryClient"), TestCategory("GetAsync"), TestCategory("HTTP.GET")]
+        public async Task GetAsyncHttpMethodTest()
+        {
+            await base.HttpMethodTestAsync(
+                (CategoryClient c) => c.GetAsync(1, false, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata),
+                HttpMethod.Get
+                );
+        }
+
+        [TestMethod, TestCategory("CategoryClient"), TestCategory("GetAsync"), TestCategory("HTTP.GET")]
+        public async Task GetAsyncUriCheckTest()
+        {
+            await base.UriTestAsync(
+              (CategoryClient p) => p.GetAsync(1, false, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata),
+              string.Format("/catalog/categories?parentCategoryId={0}&includeMetadata={1}&imageAssetDetail={2}&videoAssetDetail={3}&documentAssetDetail={4}", 1, false, 4, 4, 4));
+        }
+
+        [TestMethod, TestCategory("CategoryClient"), TestCategory("GetAsync"), TestCategory("HTTP.GET")]
+        public async Task GetAsyncHeadersTest()
+        {
+            await base.DefaultRequestHeadersTestAsync(
+                (CategoryClient c) => c.GetAsync(1, false, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata)
+            );
+        }
+
+        [TestMethod, TestCategory("CategoryClient"), TestCategory("GetAsync"), TestCategory("HTTP.GET")]
+        public async Task GetAsyncNotFoundTest()
+        {
+            var expectedExceptionResult = JsonConvert.DeserializeObject<ExceptionResult>(genericErrorResponse);
+            expectedExceptionResult.HttpStatusCode = HttpStatusCode.NotFound;
+
+            await base.APIExceptionResponseTestAsync(
+              (CategoryClient t) => t.GetAsync(1, false, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata, AssetDetailType.IncludeOnlyDefaultWithMetadata),
+              notFoundHttpResponseMessage,
+              expectedExceptionResult
+            );
+        }
+    }
+}
+        #endregion
