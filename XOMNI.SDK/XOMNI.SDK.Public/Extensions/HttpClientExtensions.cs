6f37f0930c7e316126a3c8750feed47d7783fdcb
Sezgin Ege <mail@sezginege.com>
2015-02-11 10:13:21 +0200

    Unnecessary await/async keywords removed.

--- XOMNI.SDK/XOMNI.SDK.Public/Extensions/HttpClientExtensions.cs
+++ XOMNI.SDK/XOMNI.SDK.Public/Extensions/HttpClientExtensions.cs
@@ -10,12 +10,12 @@ namespace XOMNI.SDK.Public.Extensions
 {
     public static class HttpClientExtensions
     {
-        public async static Task<HttpResponseMessage> PatchAsJsonAsync<T>(this HttpClient client, string requestUri, T value)
+        public static Task<HttpResponseMessage> PatchAsJsonAsync<T>(this HttpClient client, string requestUri, T value)
         {
             var content = new ObjectContent<T>(value, new JsonMediaTypeFormatter());
             var request = new HttpRequestMessage(new HttpMethod("PATCH"), requestUri) { Content = content };
 
-            return await client.SendAsync(request);
+            return client.SendAsync(request);
         }
     }
 }
