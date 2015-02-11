cfd4b3838fc66cadd41ab93788afd11d9921dedf
Sezgin Ege <mail@sezginege.com>
2015-02-11 12:13:42 +0200

    Range validation added.

--- XOMNI.SDK/XOMNI.SDK.Public/Clients/Company/StoreClient.cs
+++ XOMNI.SDK/XOMNI.SDK.Public/Clients/Company/StoreClient.cs
@@ -19,11 +19,13 @@ namespace XOMNI.SDK.Public.Clients.Company
         public async Task<ApiResponse<PaginatedContainer<Store>>> GetAsync(Location locationInfo, int searchDistance, int skip, int take)
 		{
             Validator.For(locationInfo, "locationInfo").IsNotNull();
+            Validator.For(locationInfo.Latitude, "Latitude").InRange(-90, 90);
+            Validator.For(locationInfo.Longitude, "Longitude").InRange(-180, 180);
             Validator.For(searchDistance, "searchDistance").InRange(0, 1);
             Validator.For(skip, "skip").IsGreaterThanOrEqual(0);
             Validator.For(take, "take").IsGreaterThanOrEqual(1);
 
-			string path = string.Format("/company/stores?locationInfo={0}&searchDistance={1}&skip={2}&take={3}", locationInfo.ToString(), searchDistance, skip, take);
+			string path = string.Format("/company/stores?locationInfo={0}&searchDistance={1}&skip={2}&take={3}", locationInfo.GetFormattedLocation(), searchDistance, skip, take);
 
 			using (var response = await Client.GetAsync(path).ConfigureAwait(false))
 			{
