66bcfedd1a0b98a7d57a063e7e706a4c69e304ef
Efe Engin <enginbehcetefe@gmail.com>
2015-02-12 16:50:30 +0200

    Client side validation added.
    Return type changed.

--- XOMNI.SDK/XOMNI.SDK.Public/Clients/OmniPlay/OmniTicketClient.cs
+++ XOMNI.SDK/XOMNI.SDK.Public/Clients/OmniPlay/OmniTicketClient.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using XOMNI.SDK.Public.Clients;
 using XOMNI.SDK.Public.Models;
 using XOMNI.SDK.Public.Models.OmniPlay;
+using XOMNI.SDK.Public.Extensions;
 
 namespace XOMNI.SDK.Public.Clients.OmniPlay
 {
@@ -17,6 +19,7 @@ namespace XOMNI.SDK.Public.Clients.OmniPlay
 
 		public async Task<ApiResponse<OmniTicketDetail>> GetTicketAsync()
 		{
+            ValidatePIIToken();
 			string path = "/omniplay/pii/ticket";
 
 			using (var response = await Client.GetAsync(path).ConfigureAwait(false))
@@ -25,24 +28,27 @@ namespace XOMNI.SDK.Public.Clients.OmniPlay
 			}
 		}
 
-        public async Task<ApiResponse<OmniSession>> GetSessionAsync()
+        public async Task<ApiResponse<List<OmniSession>>> GetSessionAsync()
 		{
+            ValidatePIIToken();
 			string path = "/omniplay/pii/session";
 
 			using (var response = await Client.GetAsync(path).ConfigureAwait(false))
 			{
-                return await response.Content.ReadAsAsync<ApiResponse<OmniSession>>().ConfigureAwait(false);
+                return await response.Content.ReadAsAsync<ApiResponse<List<OmniSession>>>().ConfigureAwait(false);
 			}
 		}
 
 		public async Task DeleteAsync()
-		{
+        {
+            ValidatePIIToken();
 			string path = "/omniplay/pii/session";
             await Client.DeleteAsync(path).ConfigureAwait(false);
 		}
 
         public async Task<ApiResponse<OmniTicketDetail>> GetAsync(Guid wishlistUniqueKey)
 		{
+            ValidatePIIToken();
 			string path = string.Format("/omniplay/wishlist/ticket?wishlistUniqueKey={0}", wishlistUniqueKey);
 
 			using (var response = await Client.GetAsync(path).ConfigureAwait(false))
@@ -53,6 +59,7 @@ namespace XOMNI.SDK.Public.Clients.OmniPlay
 
         public async Task<ApiResponse<OmniSession>> PostSessionAsync(OmniTicket omniTicket)
 		{
+            ValidatePIIToken();
 			string path = "/omniplay/pii/session";
 
 			using (var response = await Client.PostAsJsonAsync(path, omniTicket).ConfigureAwait(false))
@@ -63,6 +70,13 @@ namespace XOMNI.SDK.Public.Clients.OmniPlay
 
         public async Task<ApiResponse<WishlistImportResponse>> PostImportAsync(WishlistImportRequest wishlistImportRequest)
 		{
+            ValidatePIIToken();
+
+            if (wishlistImportRequest.GpsLocation != null)
+            {
+                Validator.For(wishlistImportRequest.GpsLocation.Latitude, "Latitude").InRange(-90, 90);
+                Validator.For(wishlistImportRequest.GpsLocation.Longitude, "Longitude").InRange(-180, 180);
+            }
 			string path = "/pii/wishlist/import";
 
             using (var response = await Client.PostAsJsonAsync(path, wishlistImportRequest).ConfigureAwait(false))
