3e81c27df313bb7b969deec89e02f6b97d4c7958
Sezgin Ege <mail@sezginege.com>
2015-02-27 10:44:22 +0200

    ItemSearchRequest inherited SearchRequest.
    - Removed duplicated properties.

--- XOMNI.SDK/XOMNI.SDK.Public/Models/Catalog/ItemSearchRequest.cs
+++ XOMNI.SDK/XOMNI.SDK.Public/Models/Catalog/ItemSearchRequest.cs
@@ -6,41 +6,16 @@ using System.Threading.Tasks;
 
 namespace XOMNI.SDK.Public.Models.Catalog
 {
-    public class ItemSearchRequest
+    public class ItemSearchRequest : SearchRequest
     {
         public int Skip { get; set; }
         public int Take { get; set; }
         public string OrderedPropertyName { get; set; }
         public string OrderBy { get; set; }
-        public int? DefaultItemId { get; set; }
-        public string RFID { get; set; }
-        public string UUID { get; set; }
-        public string Name { get; set; }
-        public string SKU { get; set; }
-        public int? CategoryId { get; set; }
-        public int? BrandId { get; set; }
-        public string Model { get; set; }
-        public string Title { get; set; }
-        public double? MinWidth { get; set; }
-        public double? MaxWidth { get; set; }
-        public double? MinHeight { get; set; }
-        public double? MaxHeight { get; set; }
-        public double? MinWeight { get; set; }
-        public double? MaxWeight { get; set; }
-        public double? MinDepth { get; set; }
-        public double? MaxDepth { get; set; }
-        public double? MinPrice { get; set; }
-        public double? MaxPrice { get; set; }
-        public int? DimensionTypeId { get; set; }
-        public int? WeightTypeId { get; set; }
-        public int? TagId { get; set; }
-        public string DelimitedDynamicAttributeValues { get; set; }
-        public bool IncludeOnlyMasterItems { get; set; }
         public string TagQuery { get; set; }
         protected virtual bool IncludeStaticNavigation { get; set; }
         protected virtual bool IncludeDynamicNavigation { get; set; }
         protected virtual bool IncludeItemsInternal { get; set; }
-        public bool? IncludePassiveItems { get; set; }
         public List<int> ItemIds { get; set; }
     }
 }
