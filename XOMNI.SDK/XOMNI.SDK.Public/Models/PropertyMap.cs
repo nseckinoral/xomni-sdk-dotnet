81226e074414f4af2677fe06770025e3f1981405
Sezgin Ege <mail@sezginege.com>
2015-03-02 19:03:08 +0200

    Type of device parameter of DeviceClient.PatchAsync changed to dynamic.
    Conflicts:
    	XOMNI.SDK/XOMNI.SDK.Public/Clients/Company/DeviceClient.cs

---
+++ XOMNI.SDK/XOMNI.SDK.Public/Models/PropertyMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOMNI.SDK.Public.Models
+{
+    public class PropertyMap
+    {
+        public PropertyInfo SourceProperty { get; set; }
+        public PropertyInfo TargetProperty { get; set; }
+    }
+}
