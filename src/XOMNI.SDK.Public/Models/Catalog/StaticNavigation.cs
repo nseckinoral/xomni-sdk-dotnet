dd306e47043d9fce96527992f60f8f52a0bdcb34
ilkayilknur <ilkay@ilkayilknur.com>
2015-03-12 15:59:56 +0200

    Price type collection added to static navigation.

--- src/XOMNI.SDK.Public/Models/Catalog/StaticNavigation.cs
+++ src/XOMNI.SDK.Public/Models/Catalog/StaticNavigation.cs
@@ -18,5 +18,6 @@ namespace XOMNI.SDK.Public.Models.Catalog
         public List<WeightMinMax> WeightRanges { get; set; }
         public List<PriceMinMax> PriceRanges { get; set; }
         public List<PriceMinMax> DiscountPriceRanges { get; set; }
+        public List<PriceType> PriceTypes { get; set; }
     }
 }
