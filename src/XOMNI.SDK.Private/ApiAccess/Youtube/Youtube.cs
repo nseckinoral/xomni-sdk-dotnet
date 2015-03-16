ecf9fe0dbe8b5108bf60d36f0515cc0b5794da08
ilkayilknur <ilkay@ilkayilknur.com>
2015-03-16 16:02:34 +0200

    youtube methods added.

---
+++ src/XOMNI.SDK.Private/ApiAccess/Youtube/Youtube.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XOMNI.SDK.Core.ApiAccess;
+using XOMNI.SDK.Core.Providers;
+using XOMNI.SDK.Model.Asset;
+using XOMNI.SDK.Model.Youtube;
+
+namespace XOMNI.SDK.Private.ApiAccess.Youtube
+{
+    public class Youtube : ApiAccessBase
+    {
+        protected override string SingleOperationBaseUrl
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        protected override string ListOperationBaseUrl
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        protected string ChannelSearchUrl
+        {
+            get { return "/private/youtube/channelsearch"; }
+        }
+
+        protected string ChannelVideoFetchUrl
+        {
+            get { return "/private/youtube/channelvideos"; }
+        }
+        protected string ChannelPlaylistFetchUrl
+        {
+            get { return "/private/youtube/playlistsearch"; }
+        }
+        protected string PlaylistVideoFetchUrl
+        {
+            get { return "/private/youtube/playlistsearch"; }
+        }
+        protected string RelateVideoUrl
+        {
+            get { return "/private/youtube/videoRelation"; }
+        }
+
+        public Task<List<Channel>> SearchChannelsAsync(string channelName, int skip, int take, ApiBasicCredential credential)
+        {
+            Dictionary<string, string> additionalParameters = new Dictionary<string, string>();
+            additionalParameters.Add("channelName", channelName);
+            additionalParameters.Add("skip", skip.ToString());
+            additionalParameters.Add("take", take.ToString());
+
+            return HttpProvider.GetAsync<List<Channel>>(GenerateUrl(ChannelSearchUrl, additionalParameters), credential);
+        }
+
+        public Task<List<Video>> GetVideosInChannelAsync(string channelId, int skip, int take, ApiBasicCredential credential)
+        {
+            Dictionary<string, string> additionalParameters = new Dictionary<string, string>();
+            additionalParameters.Add("channelId", channelId);
+            additionalParameters.Add("skip", skip.ToString());
+            additionalParameters.Add("take", take.ToString());
+
+            return HttpProvider.GetAsync<List<Video>>(GenerateUrl(ChannelVideoFetchUrl, additionalParameters), credential);
+        }
+
+        public Task<List<Playlist>> GetPlaylistsInChannelAsync(string channelId, int skip, int take, ApiBasicCredential credential)
+        {
+            Dictionary<string, string> additionalParameters = new Dictionary<string, string>();
+            additionalParameters.Add("channelId", channelId);
+            additionalParameters.Add("skip", skip.ToString());
+            additionalParameters.Add("take", take.ToString());
+
+            return HttpProvider.GetAsync<List<Playlist>>(GenerateUrl(ChannelPlaylistFetchUrl, additionalParameters), credential);
+        }
+
+        public Task<List<Video>> GetVideosInPlaylistAsync(string playlistId, int skip, int take, ApiBasicCredential credential)
+        {
+            Dictionary<string, string> additionalParameters = new Dictionary<string, string>();
+            additionalParameters.Add("playlistId", playlistId);
+            additionalParameters.Add("skip", skip.ToString());
+            additionalParameters.Add("take", take.ToString());
+
+            return HttpProvider.GetAsync<List<Video>>(GenerateUrl(PlaylistVideoFetchUrl, additionalParameters), credential);
+        }
+
+        public Task<AssetRelationMapping> RelateVideoAsync(int itemId, YoutubeAssetRelation relation, ApiBasicCredential credential)
+        {
+            Dictionary<string, string> additionalParameters = new Dictionary<string, string>();
+            additionalParameters.Add("itemId", itemId.ToString());
+
+            return HttpProvider.PostAsync<AssetRelationMapping>(GenerateUrl(RelateVideoUrl, additionalParameters), relation, credential);
+        }
+    }
+}
---
+++ src/XOMNI.SDK.Private/Youtube/YoutubeManagement.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XOMNI.SDK.Core.Management;
+using XOMNI.SDK.Core.Providers;
+using XOMNI.SDK.Model.Asset;
+using XOMNI.SDK.Model.Youtube;
+
+namespace XOMNI.SDK.Private.Youtube
+{
+    public class YoutubeManagement : ManagementBase
+    {
+        private readonly ApiAccess.Youtube.Youtube youtubeApi;
+
+        public YoutubeManagement()
+        {
+            youtubeApi = new ApiAccess.Youtube.Youtube();
+        }
+
+        public Task<List<Channel>> SearchChannelsAsync(string channelName, int skip, int take)
+        {
+            return youtubeApi.SearchChannelsAsync(channelName, skip, take, this.ApiCredential);
+        }
+
+        public Task<List<Video>> GetVideosInChannelAsync(string channelId, int skip, int take)
+        {
+            return youtubeApi.GetVideosInChannelAsync(channelId, skip, take, this.ApiCredential);
+        }
+
+        public Task<List<Playlist>> GetPlaylistsInChannelAsync(string channelId, int skip, int take)
+        {
+            return youtubeApi.GetPlaylistsInChannelAsync(channelId, skip, take, this.ApiCredential);
+        }
+
+        public Task<List<Video>> GetVideosInPlaylistAsync(string playlistId, int skip, int take)
+        {
+            return youtubeApi.GetVideosInPlaylistAsync(playlistId, skip, take, this.ApiCredential);
+        }
+
+        public Task<AssetRelationMapping> RelateVideoAsync(int itemId, YoutubeAssetRelation relation)
+        {
+            return youtubeApi.RelateVideoAsync(itemId, relation, this.ApiCredential);
+        }
+    }
+}
