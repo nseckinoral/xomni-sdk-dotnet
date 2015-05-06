4355086b2858a4cd6a559eb13a3fa9a8fdd9f53a
Gokhan Gulbiz <ggulbiz@gmail.com>
2015-05-06 12:24:42 +0300

    Mail subscription models added.

---
+++ src/XOMNI.SDK.Model.Private/Mail/MailSubscriptionDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOMNI.SDK.Model.Private.Mail
+{
+    public class MailSubscription
+    {
+        public string PIIFirstLastName { get; set; }
+        public MailSubscriptionStatus SubscriptionStatusId { get; set; }
+        public MailSubscriptionPurposeTypeEnum PurposeTypeId { get; set; }
+        public bool Subscribable { get; set; }
+    }
+}
---
+++ src/XOMNI.SDK.Model.Private/Mail/MailSubscriptionPurposeTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOMNI.SDK.Model.Private.Mail
+{
+    public enum MailSubscriptionPurposeTypeEnum
+    {
+        Wishlist = 1,
+        ShoppingCart
+    }
+}
