7d77e2b521a1eba21e1724c9c1a0499f6b349afc
ilkayilknur <ilkay@ilkayilknur.com>
2015-06-11 12:47:41 +0300

    integration endpoint creation date added.

--- src/XOMNI.SDK.Model.Management/Integration/EndpointDetail.cs
+++ src/XOMNI.SDK.Model.Management/Integration/EndpointDetail.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 namespace XOMNI.SDK.Model.Management.Integration
 {
     public class EndpointDetail
@@ -6,5 +7,6 @@ namespace XOMNI.SDK.Model.Management.Integration
         public string ServiceName { get; set; }
         public string ManagementPortalUrl { get; set; }
         public EndpointCreationStatus Status { get; set; }
+        public DateTime CreationDate { get; set; }
     }
 }
